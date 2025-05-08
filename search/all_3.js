d0ed41f4dc717e0d6728bf902784042c4dc7ecd9
fmrico <fmrico@users.noreply.github.com>
2025-05-08 17:05:15 +0000

    deploy: 1c32aa0675a1c46bb182642745d6c8063d5a025b

--- search/all_3.js
+++ search/all_3.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['get_5fdynamyc_5fmap_0',['get_dynamyc_map',['../classeasynav_1_1SimpleMapsManager.html#ad671b6dcce9f9600fb69a0ec24281b27',1,'easynav::SimpleMapsManager']]],
-  ['get_5fstatic_5fmap_1',['get_static_map',['../classeasynav_1_1SimpleMapsManager.html#a7a608228397f52826ef2d19c08213924',1,'easynav::SimpleMapsManager']]]
+  ['get_5fmaps_0',['get_maps',['../classeasynav_1_1SimpleMapsManager.html#a0cb9356fcf60d1e7afa310e36e4fb401',1,'easynav::SimpleMapsManager']]]
 ];
